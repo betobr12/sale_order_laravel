69f81a6cafc016367f0f4455ed0043cbe2818cb4
unknown <betobr12@yahoo.com.br>
2020-05-26 09:47:51 -0300

    update

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -64,16 +64,9 @@ class ItemController extends Controller
            $item = new Item();
            $item->sale_id = $request->sale_id;
 
-           if (DB::table('items')->where('product_id', $request->product_id)->count() == 0) {
+
 
            $item->product_id = $request->product_id;
-
-           }else{
-
-            Toastr::error('Item ja foi inserido','Alerta');
-            return redirect()->back();
-
-           }
 
             $resultSale = $item->sale_id;
             $resultProduct = $item->product_id;
