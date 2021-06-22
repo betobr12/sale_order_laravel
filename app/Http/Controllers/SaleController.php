1aa2ce6c030030d66c51d82f91c44a25339da57e
Roberto <betobr12@users.noreply.github.com>
2021-06-22 12:16:31 -0300

    Update SaleController.php

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -12,22 +12,12 @@ use Illuminate\Support\Facades\DB;
 
 class SaleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $sales = Sale::latest()->get();
         return view('dashboard.sale.index',compact('sales'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
         $idsale = 0;
@@ -35,19 +25,10 @@ class SaleController extends Controller
         $clientList = Client::select('id','name')->get();
         return view('dashboard.sale.create',compact('sales'),compact(['clientList','idsale']));
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(Request $request)
-
     {
-
         $productList = Product::select('id','name')->get();
-
         $this->validate($request,[
          'client_id' => 'required',
         ]);
@@ -57,22 +38,10 @@ class SaleController extends Controller
 
         $idsale = $sale->id;
         $items = $sale->items();
-
         Toastr::success('Venda Criada Com Sucesso Selecione-o abaixo para Finalizar a Venda','Successo');
-
         return redirect()->back();
-
-        //return view('dashboard.item.create',compact('sale','items'),compact(['idsale','productList']));
-
-
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show($id)
     {
         $sale = Sale::find($id);
@@ -88,91 +57,54 @@ class SaleController extends Controller
         {
             $result += $item->sale_amount * $item->sale_value;
         }
-
         return view('dashboard.sale.show',compact('sale','items'), compact(['result']));
-
     }
 
-
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-
         $sale = Sale::find($id);
-
-        if($sale->is_approved == 0){
-
-        $productList = Product::select('id','name','amount')->get();
-        $client = $sale->client()->get();
-        $clientTarget = Client::findOrFail($sale->client_id);
-        $clientList = Client::select('id','name')->get();
-        $sale = Sale::find($id);
-        $items =  $sale->items()->latest()->get();
-        $result = DB::table('items')
-                      ->where('sale_id',$id)
-                      ->get();
-        $total_price = 0;
-        foreach ($result as $item){
-            $total_price+=$item->sale_value*$item->sale_amount;
-        }
-       return  $total_price;
-         return view('dashboard.sale.edit',compact('sale','items'), compact(['clientTarget', 'clientList','productList','total_price']));
-        }else{
+        if ($sale->is_approved == 0) {
+            $productList = Product::select('id','name','amount')->get();
+            $client = $sale->client()->get();
+            $clientTarget = Client::findOrFail($sale->client_id);
+            $clientList = Client::select('id','name')->get();
+            $sale = Sale::find($id);
+            $items =  $sale->items()->latest()->get();
+            $result = DB::table('items')->where('sale_id',$id)->get();
+            $total_price = 0;
+            
+            foreach ($result as $item) {
+                $total_price+=$item->sale_value*$item->sale_amount;
+            }         
+            return view('dashboard.sale.edit',compact('sale','items'), compact(['clientTarget', 'clientList','productList','total_price']));
+        } else {
             Toastr::error('Venda aprovada não pode ser alterada','Alerta');
             return redirect()->back();
-
         }
-
-
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-            $sale = Sale::find($id);
-            $sale->client_id = $request->client_id;
-            $sale->total = $request->total;
-            $sale->is_approved = $request->is_approved;
-            $sale->save();
-
-            $sales = Sale::all();
-
-            Toastr::success('Venda gerada','Successo');
-            //return redirect()->back();
-            return redirect()->route('sale.index',compact('sales'));
-
+        $sale = Sale::find($id);
+        $sale->client_id = $request->client_id;
+        $sale->total = $request->total;
+        $sale->is_approved = $request->is_approved;
+        $sale->save();
+        $sales = Sale::all();
+        Toastr::success('Venda gerada','Successo');
+        return redirect()->route('sale.index',compact('sales'));
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy($id)
     {
         $sale = Sale::findOrFail($id);
-        if($sale->is_approved == 0){
-        $sale->delete();
-        Toastr::success('Venda excluida com sucesso','Successo');
-        return redirect()->back();
-        }else{
-        Toastr::error('Esta venda foi concluida e não pode ser excluida','Alerta');
-        return redirect()->back();
-
-    }
-
+        if ($sale->is_approved == 0) {
+            $sale->delete();
+            Toastr::success('Venda excluida com sucesso','Successo');
+            return redirect()->back();
+        } else {
+            Toastr::error('Esta venda foi concluida e não pode ser excluida','Alerta');
+            return redirect()->back();
+        }
     }
 }
